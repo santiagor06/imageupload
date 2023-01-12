903462e4d9cc1d9493dbd77ed5b1d8ccb187a835
santiago restrepo <santiago.restrepo06@hotmal.com>
2023-01-12 16:50:20 -0500

    prueba cloudinary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Button } from "react-bootstrap";
+import style from "./App.module.css";
+import Dropzone from "react-dropzone";
 
 function App() {
+  const handleOnDrop = (files) => {
+    console.log(files);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.container}>
+      <Dropzone className={style.drop} onDrop={handleOnDrop}>
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          </div>
+        )}
+      </Dropzone>
     </div>
   );
 }
