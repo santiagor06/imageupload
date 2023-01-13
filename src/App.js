b779900aa7b10b5547dffba652783cd1be8595e0
santiago restrepo <santiago.restrepo06@hotmal.com>
2023-01-12 20:21:01 -0500

    cloudinary check

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from "./logo.svg";
-import { Button } from "react-bootstrap";
 import style from "./App.module.css";
 import Dropzone from "react-dropzone";
+import axios from "axios";
 
 function App() {
-  const handleOnDrop = (files) => {
-    console.log(files);
+  const key = "598862248635565";
+  const name = "dgs9dnzy0";
+
+  const handleOnDrop = async (file) => {
+    try {
+      console.log(file);
+      const formData = new FormData();
+      formData.append("file", file[0]);
+      formData.append("upload_preset", "preset_upload");
+
+      const response = await axios.post(
+        `https://api.cloudinary.com/v1_1/${name}/image/upload`,
+        formData
+      );
+      console.log(response.data.url);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
   return (
     <div className={style.container}>
